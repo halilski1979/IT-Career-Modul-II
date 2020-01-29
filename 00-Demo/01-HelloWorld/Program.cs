4219edca6febf16744dfb6291e0fcc31cf45650b
halilski1979 <halilski1979@gmail.com>
2020-01-29 11:13:30 +0200

    Add new solution 00-Demo.sln and create new project!

---
+++ 00-Demo/01-HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<int> nums = new List<int>() { 1, 2, 3 };
+        }
+    }
+}
